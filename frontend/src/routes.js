5437fcc483552e266c4870a020bcd5bf6a7ec00e
Neto Martins <netomartins2@hotmail.com>
2020-06-21 17:54:37 -0300

    new commit

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import Profile from './pages/profile';
 import Logon from './pages/Logon';
-import Register from './pages/Register'
+import Register from './pages/Register';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes(){
     return (
@@ -11,6 +13,8 @@ export default function Routes(){
         <Switch>
             <Route path="/" exact component={Logon} />
             <Route path="/Register" component={Register} />
+            <Route path="/Profile" component={Profile} />
+            <Route path="/Incidents/new" component={NewIncident} />
 
         </Switch>
         
