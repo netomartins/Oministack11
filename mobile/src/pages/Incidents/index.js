7d7d7fa6ee63a23814ae20206c5bbab0e81fae51
Neto Martins <netomartins2@hotmail.com>
2020-06-25 01:22:52 -0300

    primeira tela do mobile pronto

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {View,Image, Text} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+import {View,FlatList, Image, Text} from 'react-native';
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function Incidents(){
+   
+    const  navigation = useNavigation();
+
+    function navigatoinDetail(){
+        navigation.navigate('Detail');
+
+    }
+   
+   
     return(
-    
+
     <View style={styles.container}>
 
         <View style={styles.header}>
@@ -17,9 +29,38 @@ export default function Incidents(){
         </View>
    
     <Text style={styles.title}>Bem Vindo</Text>
-    <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+    <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia:</Text>
 
+        <FlatList
+            data={[1 , 2, 3 ]}
+            style ={styles.incidentList}
+            keyExtractor={incident => String(incident)}
+            showsVerticalScrollIndicator={false}
+            renderItem={()=> (
 
-    </View>
+                <View style={styles.incident}>
+    
+                    <Text style={styles.incidentProperty}>ONG: </Text>
+                    <Text style={styles.incidentValue}>APAD</Text>
+    
+                    <Text style={styles.incidentProperty}>Caso: </Text>
+                    <Text style={styles.incidentValue}>Cadelinha Atropelada</Text>
+    
+                    <Text style={styles.incidentProperty}>Valor:</Text>
+                    <Text style={styles.incidentValue}>120,00</Text>
+    
+                    <TouchableOpacity 
+                    style={styles.detailButton}
+                    onPress={navigatoinDetail}>
+
+                        <Text style={ styles.detailButtonText}>Ver mais detalhes</Text>
+                        <Feather name="arrow-right" size={16} color="#E02041"/>
+                    </TouchableOpacity>
+                </View>
+            )}
+        />
+        
+        </View>
+  
     );
 }
--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -29,7 +29,50 @@ export default StyleSheet.create({
         marginTop: 48,
         color: '#13131a',
         fontWeight: 'bold'
-    }
+    },
+
+    description: {
+        fontSize:16,
+        lineHeight: 24,
+        color:'#737380'
+    },
+
+    incidentList:{
+        marginTop:32,
+    },
+
+    incident:{
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+    },
+
+    incidentProperty:{
+        fontSize: 14,
+        color: '#41414d',
+        fontWeight: 'bold'
+    },
+
+    incidentValue:{
+        marginTop: 8,
+        fontSize: 15,
+        marginBottom: 24,
+        color:"#737380",
+    },
+    detailButton:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    detailButtonText:{
+        color:'#E02041',
+        fontSize:15,
+        fontWeight: 'bold',    }
+
+
+
 
 
 
